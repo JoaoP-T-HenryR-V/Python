bfd4e32940b59fffce10c1a605ce3f4ab91d74be
João P. T. <jptirso2@gmail.com>
2026-03-26 10:37:01 -0300

    Corrigindo bugs do Ex9

--- Ex9.py
+++ Ex9.py
@@ -1,5 +1,5 @@
-altura = float(print("Digite sua altura: "))
-peso = float(print("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
 imc = peso/(altura*altura)
 if imc < 18.5:
     print("abaixo do peso normal")
