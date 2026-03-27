8906c76a877a60737f8b4c2b5b02a0ea7260956f
Henry Visone <henryvisone@gmail.com>
2026-03-27 09:19:14 -0300

    Criando e Finalizando o Ex12

---
+++ Ex12.py
@@ -0,0 +1,17 @@
+i = float(input("Digite uma Nota: "))
+maior = i
+menor = i
+media = i
+qtd = 1
+if i != -1:
+    while i != -1:
+        i = float(input("Digite uma Nota: "))
+        media += i
+        if i > maior:
+            maior = i
+        
+        qtd += 1
+media = media/qtd
+print ("A maior nota foi:", maior)
+print ("A menor nota foi:", menor)
+print ("A média das notas foi:", media)
