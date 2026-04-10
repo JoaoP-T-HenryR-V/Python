2ff7061c0323e1c6aa91dbde41a55b1ebb2762ab
jamaral33 <jpgamedev07@gmail.com>
2026-04-10 09:17:56 -0300

    Terminando o Ex16.py

---
+++ Ex16.py
@@ -0,0 +1,5 @@
+def total(qtd,v):
+    return v*qtd
+valor = float(input("Digite o valor do produto: "))
+quantidade = int(input("Digite a quantidade comprada: "))
+print ("O total da compra foi: ", total(quantidade,valor))
