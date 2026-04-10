631747c61113dfa323c00e76817068b1214bcbcb
jamaral33 <jpgamedev07@gmail.com>
2026-04-10 09:11:11 -0300

    Terminando o Ex15.py

---
+++ Ex15.py
@@ -0,0 +1,7 @@
+def maior18(idade):
+    if idade>18:
+        print ( "Usuario é maior de 18")
+    else:
+        print ( "Usuario é menor de 18" )
+idadeUsuario = int(input("Escreva sua idade: "))
+maior18 (idadeUsuario)
